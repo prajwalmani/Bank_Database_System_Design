50cdcf2fdc18748ce7549628172d1c0b1ff5c0e7
prajwalmani <prajwalmani23@gmail.com>
2021-11-28 12:13:39 -0500

    test db 2

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask,render_template,request,flash
+from flask.wrappers import Request
+import pymysql
+from pymysql import ROWID, cursors
 
 app = Flask(__name__)
 
+connection =pymysql.connect(host="localhost",user="root",passwd="",database="test")
+cursor=connection.cursor()
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def auth():
+    email=request.form['email']
+    password=request.form['password']
+
+    sql="SELECT * FROM `auth`;"
+
+    cursor.execute(sql)
+    rows=cursor.fetchall()
+    for row in rows:
+        if email==row[0] and password==row[1]:
+            print("successfull")
+        #     return "successful"
+    
+    return "not found!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
