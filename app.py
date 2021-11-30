e5a0577db041fd85ac9ae8adbcbfd1c27f185d76
prajwalmani <prajwalmani23@gmail.com>
2021-11-29 21:55:26 -0500

    refactor,duplicates and rename

--- app.py
+++ app.py
@@ -13,35 +13,49 @@ cursor=connection.cursor()
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return "Home"
+
+@app.route('/userlogin.html')
+def userlogin():
+    return render_template("userlogin.html")
 
 
-@app.route('/', methods=['GET', 'POST'])
-def auth():
+
+@app.route('/userlogin.html', methods=['GET', 'POST'])
+def userauth():
     email=request.form['email']
     password=request.form['password']
 
-    sql="SELECT * FROM `auth`;"
+    sql="SELECT * FROM `userauth`;"
 
     cursor.execute(sql)
     rows=cursor.fetchall()
     for row in rows:
         if email==row[0] and password==row[1]:
-            print("successfull")
-        #     return "successful"
-    
-    return "not found!"
+                return render_template("accountsdisp.html")
+    else:
+        flash("Wrong credentials! Try again... ")
+        return  render_template("userlogin.html")
 
-@app.route('/userregister',methods=['GET', 'POST'])
+@app.route("/employeelogin.html",methods=['GET',"POST"])
+def employeeauth():
+    return render_template("employeelogin.html")
+    
+@app.route('/employeeregister.html',methods=['GET', 'POST'])
 def userregister():
     if request.method == 'POST':
         ssn=request.form['SSN']
         branchid=request.form['Branch']
         password=request.form['Password']
-        repassword=request.form['RePassword']
+        repassword=request.form['CPassword']
+        if ssn=='' or branchid=='' or password=='' or repassword=='':
+            flash("All the fields should be filled")
+            return render_template("employeeregister.html")
+
+
         if password!=repassword:
             flash("Passwords dont match!.")
-            return render_template("userregister.html")
+            return render_template("employeeregister.html")
 
         sql="select ESSN,Branch_ID from employee where ESSN={0} and Branch_ID={1}".format(ssn,branchid)
         cursor.execute(sql)
@@ -50,7 +64,7 @@ def userregister():
             return redirect(url_for('employee'))
         else:
             flash("Employee not present in the database!")
-    return render_template("userregister.html")
+    return render_template("employeeregister.html")
 
 @app.route('/employee')
 def employee():
