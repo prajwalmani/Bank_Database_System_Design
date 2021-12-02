3580e2a4e2a9240c6e0981113015ca83fddd72b6
prajwalmani <prajwalmani23@gmail.com>
2021-12-02 18:14:02 -0500

    customer and employee register page app.py

--- app.py
+++ app.py
@@ -13,35 +13,49 @@ cursor=connection.cursor()
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return "Home"
+
+@app.route('/customerlogin.html')
+def customerlogin():
+    return render_template("customerlogin.html")
 
 
-@app.route('/', methods=['GET', 'POST'])
-def auth():
+
+@app.route('/customerlogin.html', methods=['GET', 'POST'])
+def userauth():
     email=request.form['email']
     password=request.form['password']
 
-    sql="SELECT * FROM `auth`;"
-
+    sql="SELECT * FROM `userresgister`;"
     cursor.execute(sql)
     rows=cursor.fetchall()
     for row in rows:
         if email==row[0] and password==row[1]:
-            print("successfull")
-        #     return "successful"
-    
-    return "not found!"
+                return render_template("accountsdisp.html")
+    else:
+        flash("Wrong credentials! Try again... ")
+        return  render_template("customerlogin.html")
 
-@app.route('/userregister',methods=['GET', 'POST'])
-def userregister():
+@app.route("/employeelogin.html",methods=['GET',"POST"])
+def employeeauth():
+    return render_template("employeelogin.html")
+    
+
+@app.route('/employeeregister.html',methods=['GET', 'POST'])
+def employeeregister():
     if request.method == 'POST':
         ssn=request.form['SSN']
         branchid=request.form['Branch']
         password=request.form['Password']
-        repassword=request.form['RePassword']
+        repassword=request.form['CPassword']
+        if ssn=='' or branchid=='' or password=='' or repassword=='':
+            flash("All the fields should be filled")
+            return render_template("employeeregister.html")
+
+
         if password!=repassword:
             flash("Passwords dont match!.")
-            return render_template("userregister.html")
+            return render_template("employeeregister.html")
 
         sql="select ESSN,Branch_ID from employee where ESSN={0} and Branch_ID={1}".format(ssn,branchid)
         cursor.execute(sql)
@@ -49,8 +63,33 @@ def userregister():
         for row in rows:
             return redirect(url_for('employee'))
         else:
-            flash("Employee not present in the database!")
-    return render_template("userregister.html")
+            flash("Employee not found!")
+    return render_template("employeeregister.html")
+
+@app.route('/customerregister.html',methods=['GET', 'POST'])
+def customerregister():
+    if request.method == 'POST':
+        ssn=request.form['SSN']
+        password=request.form['Password']
+        repassword=request.form['CPassword']
+        if password!=repassword:
+                flash("Passwords dont match!.")
+                return render_template("customerregister.html")
+        sql="select CSSN from customer where CSSN={0}".format(ssn)
+        cursor.execute(sql)
+        rows=cursor.fetchall()
+        for row in rows:
+            return redirect(url_for('customerlogin'))
+        else:
+            flash("Customer not found!")
+    return render_template("customerregister.html")
+
+
+
+
+@app.route('/accountsdisp.html',methods=['POST','GET'])
+def accountsdisp():
+    pass
 
 @app.route('/employee')
 def employee():
