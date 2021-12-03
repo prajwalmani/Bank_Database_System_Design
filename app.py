0624f51c030f92d1b4f016bc325dec1964550cf2
prajwalmani <prajwalmani23@gmail.com>
2021-12-02 22:35:25 -0500

    homepage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,flash,url_for,redirect
+from flask.templating import render_template_string
 from flask.wrappers import Request
 import pymysql
 from pymysql import ROWID, cursors
@@ -11,46 +12,107 @@ app.secret_key = "super secret key"
 connection =pymysql.connect(host="localhost",user="root",passwd="",database="bank")
 cursor=connection.cursor()
 
-@app.route('/')
-def home():
-    return render_template("home.html")
-
-
 @app.route('/', methods=['GET', 'POST'])
-def auth():
-    email=request.form['email']
-    password=request.form['password']
+def home():
+    if request.method=='POST':
+        customerbutton=request.form.get('customerbutton')
+        employeebutton=request.form.get('employeebutton')
+        if customerbutton=='cb':
+            return render_template('customerlogin.html')
+        elif employeebutton=='eb':
+            return render_template('employeelogin.html')
+        else:
+            pass
+    return render_template('home.html')
 
-    sql="SELECT * FROM `auth`;"
+@app.route('/customerlogin.html', methods=['GET', 'POST'])
+def customerauth():
+    if request.method == 'POST':
+        email=request.form['email']
+        password=request.form['password']
 
-    cursor.execute(sql)
-    rows=cursor.fetchall()
-    for row in rows:
-        if email==row[0] and password==row[1]:
-            print("successfull")
-        #     return "successful"
+        sql="SELECT * FROM `auth`;"
+        cursor.execute(sql)
+        rows=cursor.fetchall()
+        for row in rows:
+            if email==row[0] and password==row[1]:
+                    return render_template("customeraccountsdisp.html")
+        else:
+            flash("Wrong credentials! Try again... ")
+            return  render_template("customerlogin.html")
+
+@app.route("/employeelogin.html",methods=['GET',"POST"])
+def employeeauth():
+    if request.method == 'POST':
+        email=request.form['email']
+        password=request.form['password']
+
+        sql="SELECT * FROM `auth`;"
+        cursor.execute(sql)
+        rows=cursor.fetchall()
+        for row in rows:
+            if email==row[0] and password==row[1]:
+                return render_template("employeedisp.html")
+        else:
+            flash("Wrong credentials! Try again... ")
+            return  render_template("employeelogin.html")
     
-    return "not found!"
 
-@app.route('/userregister',methods=['GET', 'POST'])
-def userregister():
+@app.route('/employeeregister.html',methods=['GET', 'POST'])
+def employeeregister():
     if request.method == 'POST':
         ssn=request.form['SSN']
+        email=request.form['email']
         branchid=request.form['Branch']
         password=request.form['Password']
-        repassword=request.form['RePassword']
+        repassword=request.form['CPassword']
+    
         if password!=repassword:
             flash("Passwords dont match!.")
-            return render_template("userregister.html")
+            return render_template("employeeregister.html")
 
         sql="select ESSN,Branch_ID from employee where ESSN={0} and Branch_ID={1}".format(ssn,branchid)
         cursor.execute(sql)
         rows=cursor.fetchall()
         for row in rows:
+            sql=("""INSERT INTO `auth`(`email`, `password`, `SSN`) VALUES (%s,%s,%s)""")
+            values=(email,password,ssn)
+            cursor.execute(sql,values)
+            connection.commit()
             return redirect(url_for('employee'))
         else:
-            flash("Employee not present in the database!")
-    return render_template("userregister.html")
+            flash("Employee not found!")
+    return render_template("employeeregister.html")
+
+@app.route('/customerregister.html',methods=['GET', 'POST'])
+def customerregister():
+    if request.method == 'POST':
+        ssn=request.form['SSN']
+        email=request.form['email']
+        password=request.form['Password']
+        repassword=request.form['CPassword']
+        if password!=repassword:
+                flash("Passwords dont match!.")
+                return render_template("customerregister.html")
+        sql="select CSSN from customer where CSSN={0}".format(ssn)
+        cursor.execute(sql)
+        rows=cursor.fetchall()
+        for row in rows:
+            sql=("""INSERT INTO `auth`(`email`, `password`, `SSN`) VALUES (%s,%s,%s)""")
+            values=(email,password,ssn)
+            cursor.execute(sql,values)
+            connection.commit()
+            return redirect(url_for('customerlogin'))
+        else:
+            flash("Customer not found!")
+    return render_template("customerregister.html")
+
+
+
+
+@app.route('/accountsdisp.html',methods=['POST','GET'])
+def accountsdisp():
+    pass
 
 @app.route('/employee')
 def employee():
