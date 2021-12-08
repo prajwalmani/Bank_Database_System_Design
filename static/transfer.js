205dcc54732628a61d9aa913858114b0988b0fea
Kavya-umashankar <62295459+Kavya-umashankar@users.noreply.github.com>
2021-12-08 16:39:30 -0500

    tranfer update

--- static/transfer.js
+++ static/transfer.js
@@ -3,12 +3,12 @@ function onclicks(){
         if(sel.options[sel.selectedIndex].value=='cd'){
            var cdhtml=`
            <form method="post">
-   <label>Account No:</label>
-   <input type="text" name="toccountno" required/>
-   <label>Branch Id:</label>
-   <input type="number" name="tobranchid" required/>
-   <label>Amount:</label>
-   <input type="number" name="fromamount" required/>
+   <label>Account No:</label><br>
+   <input type="text" name="toccountno" required/><br>
+   <label>Branch Id:</label><br>
+   <input type="number" name="tobranchid" required/><br>
+   <label>Amount:</label><br>
+   <input type="number" name="fromamount" required/><br><br>
    <input class="btn btn-primary" type="submit" value="Submit"><br>
 </form>
            `;
@@ -17,18 +17,19 @@ function onclicks(){
         else if(sel.options[sel.selectedIndex].value=='wm'){
             var wmhtml=`
             <form method="post">
-   <label for="transcation">Select type of Account:</label>
-       <select name="adropdown" id="adropdown" required>
-           <option value="s">Savings Account</option>
-           <option value="c">Checking Account</option>
-         </select>
-         <br><br>
-   <label>Enter the amount:</label>
-   <input type="number" name="amount" required/><br>
-   <label >Enter the note(optional)</label>
-   <input type="text" name="" id=""/><br>
-   <input class="btn btn-primary" type="submit" value="Submit"><br> 
-</form>
+        <label for="transcation">Select type of Account:</label><br>
+            <select name="adropdown" id="adropdown" required>
+                <option value="s">Savings Account</option>
+                <option value="c">Checking Account</option>
+              </select>
+              <br><br>
+        <label>Enter the amount:</label><br>
+        <input type="number" name="amount" required/><br>
+        <br>
+        <label >Enter the note(optional)</label><br>
+        <input type="text" name="" id=""/><br><br>
+        <input class="btn btn-primary" type="submit" value="Submit"><br> <br>
+     </form>
             `;
             document.getElementById('dv').innerHTML=wmhtml;
 
@@ -36,15 +37,17 @@ function onclicks(){
        else if(sel.options[sel.selectedIndex].value=='csd'){
         var csdhtml=`
         <form method="post">
-    <label >Select the type of account:</label>
-    <input type="radio" name="raccount" id="rsavings" value="s">
-    <label>Savings Account</label>
-    <input type="radio" name="raccount" id="rchecking" value="c">
-    <label>Checking Account</label>
-    <label>Enter the amount:</label>
-    <input type="number" name="eamount" id="eamount" required/>
-    <button type="submit" name="waitsubmit" value="waitsubmit">Submit</button>
-</form>
+        <label >Select the type of account:</label><br>
+        <input type="radio" name="raccount" id="rsavings" value="s">
+        <label>Savings Account</label>
+        <br>
+        <input type="radio" name="raccount" id="rchecking" value="c">
+        <label>Checking Account</label><br><br>
+        <label>Enter the amount:</label><br>
+        <input type="number" name="eamount" id="eamount" required/><br>
+        <br>
+        <button type="submit" name="waitsubmit" value="waitsubmit">Submit</button>
+    </form>
         `;   
         document.getElementById('dv').innerHTML=csdhtml;
        }
